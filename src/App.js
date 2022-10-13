d1f9458bc9365ef786938700bf98ed529db0aaab
Jack <fakegenki@gmail.com>
2022-10-13 16:40:06 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
 import './App.css';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:type/" element={<HomePage />} />
+          <Route path="/:type/:id/" element={<PetDetailsPage />} />
+          <Route path="/search/" element={<SearchPage />} />
+          <Route path="/pet-details-not-found" element={<PetDetailsNotFound />} />
+        </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+//import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const { worker } = require('./mocks/browser');
+worker.start();
+
+/*const root = document.getElementById('root');
 root.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
-);
+  
+); */
+
+/*const container = document.getElementById('root');
+const plant = createRoot(container);
+plant.render(<App />); */
+createRoot(document.getElementById('root')).render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
